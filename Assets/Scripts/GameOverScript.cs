b717dab04179351cf1e0ac46bfd62b4b88c1bb21
Emil Forsén <emil.forsen@klash.studio>
2022-11-04 08:54:32 +0100

    Added highscore

--- Assets/Scripts/GameOverScript.cs
+++ Assets/Scripts/GameOverScript.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameOverScript : MonoBehaviour
 {
     [SerializeField] private GameObject panel;
     [SerializeField] private CharacterSounds grannySounds;
+   
     void Start()
     {
         panel.SetActive(false);
     }
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.K))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             GameOver();
         }
@@ -20,6 +22,7 @@ public class GameOverScript : MonoBehaviour
     public void GameOver()
     {
         panel.SetActive(true);
+        
         grannySounds.audioSource.PlayOneShot(grannySounds.audioLibrary[0]);
 
     }
--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -6,16 +6,24 @@ using UnityEngine;
 public class ScoreManager : MonoBehaviour
 { 
     [SerializeField] private TMP_Text scoreText;
+    public TMP_Text highscore;
 
     // Start is called before the first frame update
     void Start()
     {
         scoreText.text = "Score: " + 0;
+        highscore.text = "Highscore " + PlayerPr efs.GetInt("Highscore", 0);
     }
 
     public void UpdateScore(int playerScore)
     {
         scoreText.text = "Score " + playerScore;
+        
+        if (playerScore > PlayerPrefs.GetInt("Highscore", 0))
+        {
+            PlayerPrefs.SetInt("Highscore", playerScore);
+            highscore.text = "Highscore " + playerScore;
+        }
     }
 
     // Update is called once per frame
